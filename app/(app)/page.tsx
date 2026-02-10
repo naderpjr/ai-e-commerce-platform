696dafc823c981259ff5a39cfdf914c095ad6ccd
Nader PJR <pjrnader@gmail.com>
2026-02-10 19:17:43 +0430

    queries added

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,7 +1,16 @@
+import { sanityFetch } from "@/sanity/lib/live";
+import { ALL_CATEGORIES_QUERY } from "@/sanity/queries/categories";
 
 
+export default async function Home() {
 
-export default function Home() {
+  // Fetch categories for filter sidebar
+  const { data: categories } = await sanityFetch({
+    query: ALL_CATEGORIES_QUERY,
+  });
+
+  console.log(categories);
+
   return (
     <div className="">
       <h1>Hello World</h1>
